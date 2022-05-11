06355c32cc44508fd349490b29e238e8a1e69b77
Javier311098 <javiermartinez311098@gmail.com>
2022-05-11 16:42:01 -0500

    Primer commit

---
+++ models/sockets.js
@@ -0,0 +1,19 @@
+class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.socketEvents();
+  }
+
+  socketEvents() {
+    //On connection
+    this.io.on("connection", (socket) => {
+      // Escuchar evento: mensaje-cliente
+      socket.on("mensaje-cliente", (data) => {
+        console.log(data);
+        this.io.emit("mensaje-server", data);
+      });
+    });
+  }
+}
+
+module.exports = Sockets;
